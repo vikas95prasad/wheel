6083c6861eca385869b0624114b1a468ed3207d1
Vikas Prasad <vikas.prasad@bigbinary.com>
2021-05-02 20:04:15 +0530

    Add new columns in notes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_122313) do
+ActiveRecord::Schema.define(version: 2021_05_02_074522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2020_10_14_122313) do
     t.uuid "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
+    t.datetime "due_date"
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
