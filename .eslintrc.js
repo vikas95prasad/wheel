edd11f7aab3280757cb919b68215589e7fa07c75
Vikas Prasad <vikas.prasad@bigbinary.com>
2021-05-05 13:34:00 +0530

    Remove exceptions in rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     "react"
   ],
   "rules": {
-    "indent": ["error", 2, { "ignoredNodes": ["ConditionalExpression"] }],
+    "indent": ["error", 2],
     "semi": ["error", "always"],
     "no-console": "error",
     "import/prefer-default-export": "off",
